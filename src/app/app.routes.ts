0b539b195f94103be00bc57d3e21c25aa97a7da0
Angel_PC <araozangel842@gmail.com>
2024-01-04 18:11:23 +0200

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'todos',
+    loadComponent: () => {
+      return import('./components/todo/todo.component').then(
+        (m) => m.TodoComponent
+      );
+    },
+  },
+  {
+    path: '',
+    redirectTo: '/todos',
+    pathMatch: 'full',
+  },
+];
