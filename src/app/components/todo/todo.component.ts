9d093bce6c1a77c87e99cc303790903c140db422
Angel_PC <araozangel842@gmail.com>
2024-01-05 13:56:24 +0200

    Folder structure

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../model/Todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
+  title: string = 'Todo';
 
+  todos: Todo[]  = [];
+
+  constructor() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Learn Angular',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Learn React',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Learn Vue',
+        completed: false,
+      },
+    ];
+  }
+
+  remove(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 }
