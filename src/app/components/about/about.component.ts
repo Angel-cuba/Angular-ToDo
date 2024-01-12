f0708d8bb529a67b6f49e885da7bccb007a2a32a
Angel_PC <araozangel842@gmail.com>
2024-01-12 23:21:50 +0200

    Using custom components

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SmallTitleComponent } from '../content/small-title/small-title.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SmallTitleComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { BigTitleComponent } from '../content/big-title/big-title.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, BigTitleComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
