19853fa41e66048dd268e2fb8c29c5278bea7516
Angel_PC <araozangel842@gmail.com>
2024-01-12 23:22:30 +0200

    Routes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,24 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { routes } from '../../app.routes';
+import { SmallTitleComponent } from '../content/small-title/small-title.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SmallTitleComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
-
   constructor(private router: Router) {}
-
 
   public navItems: any = routes
     .map((route) => route.children ?? [])
     .flat()
     .filter((route) => route.path !== '')
-    .filter((route) => !route.path?.includes(':'))
-
+    .filter((route) => !route.path?.includes(':'));
 }
