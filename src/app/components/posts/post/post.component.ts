44a0df5d74632c8cad94baa8e0bd97d720c1fb6c
Angel_PC <araozangel842@gmail.com>
2024-01-14 14:58:17 +0200

    Post

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../../interfaces/Post';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDividerModule } from '@angular/material/divider';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatButtonModule} from '@angular/material/button';
+import { SmallTitleComponent } from '../../content/small-title/small-title.component';
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatDividerModule,
+    FontAwesomeModule,
+    SmallTitleComponent,
+    MatButtonModule,
+  ],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
 export class PostComponent {
+  @Input() post: Post | undefined;
+  constructor() {}
 
+  likePost(postId: string | undefined) {
+    console.log('🚀 ~ PostComponent ~ likePost ~ postId', postId);
+  }
 }
