28a1423bd59d41a70c5eb1041ba0543ae3937146
Angel_PC <araozangel842@gmail.com>
2024-01-14 14:57:59 +0200

    Fetch posts data

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { PostServiceService } from '../../services/posts/post-service.service';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { SmallTitleComponent } from '../content/small-title/small-title.component';
+import { PostComponent } from './post/post.component';
+import {MatProgressBarModule } from '@angular/material/progress-bar';
+import { BigTitleComponent } from '../content/big-title/big-title.component';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, SmallTitleComponent, PostComponent, MatProgressBarModule, BigTitleComponent],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
@@ -14,8 +18,4 @@ export class PostsComponent {
   public service = inject(PostServiceService);
 
   constructor() {}
-
-  dataPosts() {
-    return this.service.loadPosts();
-  }
 }
