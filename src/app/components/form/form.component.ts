fdb2066248577ed8ef058a4f76410f9770499290
Angel_PC <araozangel842@gmail.com>
2024-01-16 13:06:14 +0200

    Form to add/edit a post

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 export class FormComponent {
+  public isEditing: boolean = false;
+  public id: string = '';
+  public route = inject(ActivatedRoute);
 
+  public form: FormGroup = this.formBuilder.group({
+    title: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(20)],
+    ],
+    body: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+    ],
+    authorId: [''],
+    image: ['', [Validators.required, Validators.pattern('https?://.+')]],
+    tags: [''],
+  });
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.route.params.subscribe((params) => {
+      const postId = params['id'];
+      if (postId) {
+        this.isEditing = true;
+        this.id = postId;
+      }
+    });
+  }
+
+  savePost() {
+    if (!this.form.valid) {
+      console.log('Formulario no válido:', this.form.value);
+    }
+    const post = {
+      ...this.form.value,
+      authorId: '123',
+    }
+      console.log("🚀 ~ FormComponent ~ savePost ~ post:", post)
+  }
 }
