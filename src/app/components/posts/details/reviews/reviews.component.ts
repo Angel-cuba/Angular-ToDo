13e92c0a29f8e5d65e663fdc790e0ee9eaf10701
Angel_PC <araozangel842@gmail.com>
2024-01-20 23:06:54 +0200

    Fetch reviews

--- src/app/components/posts/details/reviews/reviews.component.ts
+++ src/app/components/posts/details/reviews/reviews.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { ReviewIds } from '../../../../interfaces/Post';
+import { CommonModule } from '@angular/common';
+import { PostService } from '../../../../services/posts/post-service.service';
 
+type ReviewsResponse = {
+  data: ReviewIds[];
+  message: string;
+  status: string;
+}
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss',
 })
-export class ReviewsComponent {
-  @Input() reviews: any;
+export class ReviewsComponent implements OnInit {
+  public reviews: ReviewsResponse[] | any= [];
+  @Input() id: string = '';
 
-  constructor() {}
+  constructor(private service: PostService) {}
+
+  ngOnInit(): void {
+    this.service.getReviewsByPostId(this.id).subscribe((reviews) => {
+      this.reviews = reviews.data;
+    });
+  }
 }
