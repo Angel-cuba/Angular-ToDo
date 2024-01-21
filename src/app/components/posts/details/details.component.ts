4c77f00c679b18dd52032065fd1c1d4af5f6e9f7
Angel_PC <araozangel842@gmail.com>
2024-01-21 23:35:42 +0200

    Ongoing work...
    - Create a form to add a new review.
    - Add styles.

--- src/app/components/posts/details/details.component.ts
+++ src/app/components/posts/details/details.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { PostService } from '../../../services/posts/post-service.service';
 import { PostComponent } from '../post/post.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { Post, PostResponse } from '../../../interfaces/Post';
+import { ToastrService } from 'ngx-toastr';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterModule, PostComponent, ReviewsComponent],
+  imports: [
+    RouterModule,
+    PostComponent,
+    ReviewsComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
@@ -17,11 +33,24 @@ export class DetailsComponent implements OnInit {
   public id: string = '';
   public post: Post | any = {};
   public reviews: any = [];
+  public isEditing: boolean = false;
 
-  public route = inject(ActivatedRoute);
-  public postService = inject(PostService);
+  public body: string = '';
 
-  constructor(private router: Router) {}
+  public form: FormGroup = this.formBuilder.group({
+    body: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(200)],
+    ],
+  });
+
+  constructor(
+    private router: Router,
+    private toaster: ToastrService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -44,18 +73,43 @@ export class DetailsComponent implements OnInit {
       });
     });
   }
-  //TODO: Añadir alguna libreria para mostrar popups
 
   // Función que procesa el post
   private processPost() {
     console.log('Post obtenido:', this.post);
     // Puedes realizar cualquier acción adicional basada en la información del post aquí
+    this.toaster.info(
+      'Let us know what you think about this post!',
+      this.post.title,
+      {
+        timeOut: 2000,
+        messageClass: 'toast-message',
+      }
+    );
   }
 
   // Función que procesa las reviews
   private processReviews(reviews: any) {
     console.log('Reviews obtenidas:', reviews);
     // Puedes realizar cualquier acción adicional basada en las reviews aquí
+  }
+
+  saveReview() {
+    if (!this.form.valid) {
+      return;
+    }
+    const review = {
+      body: this.form.value.body,
+      authorId: '272734628828jd83',
+    };
+    console.log('🚀 ~ DetailsComponent ~ saveReview ~ review:', review);
+
+    // Llamada a la función que guarda la review
+    // this.postService.createReview(review).subscribe((review) => {
+    // Llamada a la función que procesa la review (por ejemplo, mostrar un modal)
+    //this.processReview(review);
+    // });
+    this.form.reset();
   }
 
   goBack() {
