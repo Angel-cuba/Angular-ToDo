e23f3df20da1710c50eae010db794a0c433503b1
Angel_PC <araozangel842@gmail.com>
2024-01-22 16:04:34 +0200

    Create method to create a review

--- src/app/services/review/review.service.ts
+++ src/app/services/review/review.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { PostResponse } from '../../interfaces/Post';
+import { Observable } from 'rxjs';
+import { ReviewInterfaceResponse } from '../../interfaces/Reviews';
 
+type Review = {
+  body: string;
+  authorId: string;
+};
+interface createReview {
+  data: string;
+  message: string;
+  status: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReviewService {
+  url: string = environment.localUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getReviewsByPostId(id: string): Observable<ReviewInterfaceResponse> {
+    return this.http.get<ReviewInterfaceResponse>(this.url + `reviews/all/${id}`);
+  }
+
+  createReview(postId: string, review: Review) {
+    return this.http.post<createReview>(
+      this.url + `reviews/create/${postId}`,
+      review
+    );
+  }
 }
