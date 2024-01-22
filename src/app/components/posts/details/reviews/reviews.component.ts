d3b1976337c9a7c8ba2532212b83e57b8492a14f
Angel_PC <araozangel842@gmail.com>
2024-01-22 16:05:35 +0200

    Refactoring.
    - It's not needed to call the method directly from the template, because is receiving the data from the parent component.

--- src/app/components/posts/details/reviews/reviews.component.ts
+++ src/app/components/posts/details/reviews/reviews.component.ts
@@ -1,13 +1,8 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { ReviewIds } from '../../../../interfaces/Post';
+import { Component, Input } from '@angular/core';
+import { Review } from '../../../../interfaces/Reviews';
 import { CommonModule } from '@angular/common';
-import { PostService } from '../../../../services/posts/post-service.service';
 
-type ReviewsResponse = {
-  data: ReviewIds[];
-  message: string;
-  status: string;
-}
+
 @Component({
   selector: 'app-reviews',
   standalone: true,
@@ -15,15 +10,8 @@ type ReviewsResponse = {
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss',
 })
-export class ReviewsComponent implements OnInit {
-  public reviews: ReviewsResponse[] | any= [];
-  @Input() id: string = '';
+export class ReviewsComponent {
+  @Input() reviews: Review[] = [];
 
-  constructor(private service: PostService) {}
-
-  ngOnInit(): void {
-    this.service.getReviewsByPostId(this.id).subscribe((reviews) => {
-      this.reviews = reviews.data;
-    });
-  }
+  constructor() {}
 }
