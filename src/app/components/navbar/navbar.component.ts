47a127f6e7c286df63386c7bb2e20145c421b597
Angel_PC <araozangel842@gmail.com>
2024-03-05 20:44:41 +0200

    Implement Subscription method to handle logged value.
    - Display user img if user is logged in.
    - Improve scss for navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { routes } from '../../app.routes';
 import { SmallTitleComponent } from '../content/small-title/small-title.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../services/auth/auth.service';
-import { LoginResponse, newUser } from '../../interfaces/User';
+import {  newUser } from '../../interfaces/User';
 import { ToastrService } from 'ngx-toastr';
+import { Observable, Subscription } from 'rxjs';
+import { UserInLocalStorage } from '../../services/auth/session';
 
 @Component({
   selector: 'app-navbar',
@@ -22,10 +24,11 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public openMenu: boolean = false;
   public isUserLogged: boolean = false;
-  public token: string | null = localStorage.getItem('token') as string;
+  public userImage: string | undefined = '';
+  public subscription: Subscription = new Subscription();
 
   constructor(
     private router: Router,
@@ -43,11 +46,24 @@ export class NavbarComponent {
     .filter((route) => !route.path?.includes('login'));
 
   ngOnInit(): void {
-    this.isUserLogged = this.authService.checkAuth();
+    this.subscription = this.authService.getSessionStatus().subscribe({
+      next: (isAuthenticated: UserInLocalStorage) => {
+        this.isUserLogged = isAuthenticated.isLoggedIn;
+        this.setUserImage();
+      },
+      error: (err: any) => {
+        console.error('🚀 ~ NavbarComponent ~ ngOnInit ~ error', err);
+      },
+    });
   }
 
   goToLogin(): void {
-    this.router.navigate(['hero/login']);
+    if (this.isUserLogged) {
+      this.authService.logout();
+      this.isUserLogged = false;
+    } else {
+      this.router.navigate(['hero/login']);
+    }
   }
 
   public toggleMenu(): void {
@@ -58,49 +74,9 @@ export class NavbarComponent {
     this.openMenu = false;
   }
 
-  loginUser() {
-    if (this.isUserLogged) {
-      this.authService.logout();
-      this.isUserLogged = false;
-    } else {
-      const user = {
-        email: 'user2@gmail.com',
-        password: '123456',
-      };
-      this.authService.login(user).subscribe((result: LoginResponse) => {
-        console.log(
-          '🚀 ~ NavbarComponent ~ this.authService.login ~ result:',
-          result
-        );
-        localStorage.setItem('token', result.data.token);
-        localStorage.setItem('user', JSON.stringify(result.data.user));
-        this.isUserLogged = true;
-      });
-    }
-  }
-
-  registerUser() {
-    const user: newUser = {
-      email: 'user11@gmail.com',
-      username: 'toto',
-      password: '123456',
-      image: 'https:www',
-      bio: 'Im goo guy',
-      github: 'github',
-      linkedin: 'linkedin',
-    };
-    this.authService.register(user).subscribe({
-      next: (user) => {
-        console.log('🚀 ~ NavbarComponent ~ regiterUser ~ user', user);
-      },
-      error: (error) => {
-        console.log('🚀 ~ NavbarComponent ~ regiterUser ~ error', error);
-      },
-    });
-  }
-
   goToProfile() {
-    if (!this.isUserLogged && !this.token) {
+    const session = localStorage.getItem('session') ?? '';
+    if (!session) {
       this.toast.error(
         'You need to be logged in to access to your profile',
         'We need to know who you are!'
@@ -108,5 +84,13 @@ export class NavbarComponent {
       return;
     }
     this.router.navigate(['/hero/profile']);
+  }
+
+  setUserImage() {
+    const session = localStorage.getItem('session') ?? '';
+    if (session) {
+      const sessionData = JSON.parse(session);
+      this.userImage = sessionData.data.user.image;
+    }
   }
 }
