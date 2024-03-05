aa15bc902824d10a401b570a70e52b4fb7c59ec1
Angel_PC <araozangel842@gmail.com>
2024-03-05 20:47:46 +0200

    Refactoring. Ongoing work....
    - Improve checkAuth method.
    - Delete unnecessary auth methods.

--- src/app/guards/auth-guards.ts
+++ src/app/guards/auth-guards.ts
@@ -1,45 +1,24 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
 function checkAuth(): boolean | Observable<boolean> {
   const toast = inject(ToastrService);
-  const token = localStorage.getItem('token');
+  const session = localStorage.getItem('session') ?? '';
+  const sessionData = JSON.parse(session);
 
-  const isValidToken = token !== null && token !== undefined;
-
-  if (!isValidToken) {
-    toast.error('You have to login first', 'Unauthorized', {
+  if (sessionData.data.token) {
+    return true;
+  } else {
+    toast.error('Your session has expired', 'Unauthorized', {
       timeOut: 1500,
     });
     return false;
   }
-  return true;
-}
-
-function loggedUser(): boolean | Observable<boolean> {
-  const router = inject(Router);
-  const authService = inject(AuthService);
-  const toastr = inject(ToastrService);
-
-  const userToken = authService.checkAuth();
-
-  if (!userToken) {
-    toastr.error('Your session has expired', 'Unauthorized', {
-      timeOut: 1500,
-      })
-    router.navigate(['/hero']);
-    return false;
-  }
-  return true;
 }
 
 export const AuthGuard = () => {
   return checkAuth();
 };
 
-export const LoggedUserGuard = () => {
-  return loggedUser();
-};
