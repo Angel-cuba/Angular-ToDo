b452f4ba23a191ae8b4c4722bc60627d5ada483e
Angel_PC <araozangel842@gmail.com>
2024-03-07 16:33:44 +0200

    Refactoring
    - Giving styles to the error message.
    - Delete unnecessary code.
    - Add styles to the loading message.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit(): void {}
 }
