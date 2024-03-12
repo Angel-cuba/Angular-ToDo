b0937fb5f0c9d3c8c75f014e5beabb90d77dda9e
Angel_PC <araozangel842@gmail.com>
2024-03-13 01:35:50 +0200

    Summary:
    - Just review owner can edit or delete review.
    - Check user status before edit or delete review.
    - Load userId from method.

--- src/app/components/posts/details/reviews/reviews.component.ts
+++ src/app/components/posts/details/reviews/reviews.component.ts
@@ -3,6 +3,8 @@ import { Review } from '../../../../interfaces/Reviews';
 import { CommonModule } from '@angular/common';
 import { ReviewService } from '../../../../services/review/review.service';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { UserInLocalStorage } from '../../../../services/auth/session';
 
 @Component({
   selector: 'app-reviews',
@@ -15,15 +17,32 @@ export class ReviewsComponent {
   @Input() reviews: Review[] = [];
   @Input() postId: string = '';
   @Output() reviewIdToEdit = new EventEmitter<string>();
+  public isUserLoggedIn: boolean = false;
+  public userId: string = '';
 
   public userPicture: string =
     'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020658/samples/bike.jpg';
   userName: string = 'John Doe';
 
-  public review: Review | any = {};
+
   isEditing: boolean = false;
 
-  constructor(private reviewService: ReviewService, private toaster: ToastrService) {}
+  constructor(
+    private reviewService: ReviewService,
+    private toaster: ToastrService,
+    private authService: AuthService
+  ) {
+    this.authService.getSessionStatus().subscribe({
+      next: (response: UserInLocalStorage) => {
+        this.isUserLoggedIn = response.isLoggedIn;
+        this.userId = this.authService.getUserId();
+      },
+      error: (error) => {
+        this.isUserLoggedIn = false;
+        this.toaster.error('Error getting user status', 'Error!');
+      },
+    });
+  }
 
   likeReview(reviewId: string) {
     console.log(reviewId);
@@ -35,16 +54,28 @@ export class ReviewsComponent {
 
   editReview(reviewIdToEdit: string) {
     if (reviewIdToEdit === undefined) return;
+
+    if (this.isUserLoggedIn === false) {
+      this.toaster.error(
+        'You need to be logged in to edit a review',
+        'Stop there!'
+      );
+      return;
+    }
     this.reviewIdToEdit.emit(reviewIdToEdit);
   }
 
   deleteReview(reviewIdToEdit: string) {
     if (reviewIdToEdit === undefined) return;
+
+    const token = this.authService.getToken();
     this.reviewService
-      .deleteReview(this.postId, reviewIdToEdit, '272734628828jd83')
+      .deleteReview(this.postId, reviewIdToEdit, this.userId, token)
       .subscribe({
         next: (response) => {
-          const filteredReviews = this.reviews.filter(review => review.id !== reviewIdToEdit);
+          const filteredReviews = this.reviews.filter(
+            (review) => review.id !== reviewIdToEdit
+          );
           this.toaster.success(response.message);
           this.reviews = filteredReviews;
         },
