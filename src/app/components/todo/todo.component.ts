4503319dd9500688f6dbc60d2c547e216380852f
Angel_PC <araozangel842@gmail.com>
2024-03-14 22:01:34 +0200

    Summary:
    - Call methods for user id and session status(if user is authenticated).
    - Filtering posts based of user id and session status.

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PostService } from '../../services/posts/post-service.service';
 import { Post } from '../../interfaces/Post';
 import { PostComponent } from '../posts/post/post.component';
+import { UserInLocalStorage } from '../../services/auth/session';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo',
@@ -13,18 +18,19 @@ import { PostComponent } from '../posts/post/post.component';
   styleUrl: './todo.component.scss',
 })
 export class TodoComponent implements OnInit {
-  title: string = 'Todo';
-  userId = '272734628828jd83';
-
-  showTodo = false
-
-  todos: Todo[]  = [];
-
+  public title: string = 'Todo';
+  public todos: Todo[] = [];
   public posts: Post[] = [];
+  public userPosts = inject(PostService);
+  public showTodo: boolean = false;
+  public isUserLogged: boolean = false;
+  public subscription: Subscription = new Subscription();
 
-  public userPosts = inject(PostService)
-
-  constructor() {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toaster: ToastrService
+  ) {
     this.todos = [
       {
         id: 1,
@@ -42,19 +48,41 @@ export class TodoComponent implements OnInit {
         completed: false,
       },
     ];
+
+    this.subscription = this.authService.getSessionStatus().subscribe({
+      next: (response: UserInLocalStorage) => {
+        this.isUserLogged = response.isLoggedIn;
+      },
+      error: (error) => {
+        this.toaster.error('Error verifying authentication', 'Error');
+        this.router.navigate(['hero/login']);
+      },
+    });
   }
 
   ngOnInit(): void {
-    this.userPosts.loadPostsByUserId(this.userId).subscribe(data => {
-      this.posts = data.data;
+    const userId = this.authService.getUserId();
+    if (!this.isUserLogged) {
+      this.toaster.error(
+        'You have to login before...',
+        'Yooooooo! Something went wrong!'
+      );
+    } else {
+      this.userPosts.loadPostsByUserId(userId).subscribe({
+        next: (response) => {
+          this.posts = response.data;
+        },
+        error: (error) => {
+          this.toaster.error('Error loading posts', 'Error');
+        },
+      });
     }
-    )
   }
   remove(id: number): void {
     this.todos = this.todos.filter((todo) => todo.id !== id);
   }
 
   switchTo() {
-    this.showTodo = !this.showTodo
+    this.showTodo = !this.showTodo;
   }
 }
